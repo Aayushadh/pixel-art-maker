67ada0d72211332921411fa50cd1d59d7ffe6434
Aayushadh <ayushharwani2011@gmail.com>
2020-09-16 22:04:18 +0530

    firstOne

---
+++ designs.js
@@ -0,0 +1,48 @@
+
+
+// Grid generating part
+var submit = document.querySelector('#submitForm');
+
+submit.addEventListener('click', function() {
+
+    var height, width;
+    height = document.querySelector('#inputHeight');
+    width = document.querySelector('#inputWidth');
+    console.log(height.value);
+    console.log(width.value);
+
+    var table = document.querySelector('table');
+    removeAllChildNodes(table);
+    for (let i = 0; i < height.value; i++) {
+        let row = document.createElement('tr');
+        for (let j = 0; j < width.value; j++) {
+            let col = document.createElement('td');
+            row.appendChild(col);
+        }
+        table.appendChild(row);
+
+    }
+
+
+
+});
+
+
+// when the value is submitted again it is neccessary to remove old one grid
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+
+
+// color filler part using target or event delegation 
+
+var table = document.querySelector('table');
+
+table.addEventListener('click', function (event) {
+    var color = document.querySelector('#colorPicker');
+    event.target.style.backgroundColor = color.value;
+
+});
